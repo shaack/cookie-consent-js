4cb4c6e05e5a6e569d1300fa5442826c246b562a
Stefan Haack <shaack@gmail.com>
2020-08-31 22:26:31 +0200

    removed ';'

--- src/cookie-consent.js
+++ src/cookie-consent.js
@@ -4,7 +4,7 @@
  * License: MIT, see file 'LICENSE'
  */
 
-;function CookieConsent(props) {
+function CookieConsent(props) {
     var self = this
     this.props = {
         cookieName: "cookie-consent-accept-all",

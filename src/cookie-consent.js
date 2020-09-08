350c579d73d433f0aa4d7e916290ebd1fd45b4db
Stefan Haack <shaack@gmail.com>
2020-09-08 20:18:13 +0200

    blockAccess Mode

--- src/cookie-consent.js
+++ src/cookie-consent.js
@@ -13,7 +13,7 @@ function CookieConsent(props) {
         privacyPolicyUrl: "privacy-policy.html",
         autoShowModal: true, // disable autoShowModal on the privacy policy page, to make that page readable
         lang: navigator.language, // the language, in which the modal is shown
-        blockAccess: false, // set "true" to block the access to the website before choosing a cookie configuration
+        blockAccess: false, // set "true" to block the access to the website _before_ choosing a cookie configuration
         content: { // the content in all needed languages
             de: {
                 title: "Cookie-Einstellungen",
